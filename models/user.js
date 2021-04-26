18b3abeba41b40f5e4294110812ee4cf9d9761ec
PatriContreras <patri.contreras91@gmail.com>
2021-04-26 16:25:18 +0200

    login and unique user created.

--- models/user.js
+++ models/user.js
@@ -1,4 +1,15 @@
-var express = require('express');
-var router = express.Router();
 
-module.exports = router;
+const getByEmail = (email) => {
+    return new Promise((resolve, reject) => {
+        db.query('SELECT * FROM users WHERE email = ?', [email], (err, result) => {
+            if (err) {
+                return reject(err);
+            }
+            if (result.length === 0) return resolve(null)
+            resolve(result[0]);
+        })
+    })
+}
+
+
+module.exports = { getByEmail }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,3 +1,40 @@
+const { getByEmail } = require('../../models/user');
+
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const dayjs = require('dayjs');
 const router = require('express').Router();
+
+router.post('/login', async (req, res) => {
+    const user = await getByEmail(req.body.email);
+    console.log(user);
+    if (user) {
+        const samePw = bcrypt.compareSync(req.body.password, user.password);
+        //console.log(req.body.password);
+        //console.log(user.password);
+        console.log('comparacion', samePw);
+        if (samePw) {
+            res.json({
+                success: 'Login correcto',
+                token: createToken(user),
+                id: user.id
+            })
+        } else {
+            res.json({ error: 'error en contraseña' })
+        }
+    } else {
+        res.json({ error: 'error en email' })
+    }
+});
+
+function createToken(user) {
+    const data = {
+        userId: user.id,
+        caduca: dayjs().add(60, 'minutes').unix()
+    }
+
+    const token = jwt.sign(data, 'f1966f33ba32d7e4a4e1bfefa6d27a7f7e2b1e636362ff0cbdd2f29b3f6cef67')
+    return token;
+}
 
 module.exports = router;
