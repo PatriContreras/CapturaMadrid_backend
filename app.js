f6de8559cd1fc220c074c59db6469c49555925f4
PatriContreras <patri.contreras91@gmail.com>
2021-05-11 21:01:04 +0200

    Changed payload to larger one

--- app.js
+++ app.js
@@ -19,8 +19,10 @@ app.set('view engine', 'pug');
 
 app.use(cors());
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
